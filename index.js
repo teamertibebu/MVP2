908986e9e20325ac0f24ace0d6998f9fe549c6a5
Teamer Tibebu <teamertibebu@gmail.com>
2020-11-16 22:24:59 -0600

    only searches deleted scenes

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ app.use(cors());
 
 app.get('/', (req, res) => {});
 app.post('/', (req, res) => {
-  const query = req.body.showTitle;
+  const query = req.body.showTitle + 'deletedscenes';
+  console.log(query);
   const URL = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyC7kvwZKyLjxGDmexve29WSaybKEPc42gg&type=video&part=snippet&maxResults=10&q=${query}`;
 
   axios
     .get(URL)
     .then((response) => {
-      console.log(response);
       res.send(response.data.items);
     })
     .catch((err) => {
